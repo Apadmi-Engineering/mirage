7c41437b0365ab2b11e68408f2a67062317964f9
TomRHandcock <33254448+TomRHandcock@users.noreply.github.com>
2025-10-04 19:23:05 +0100

    feat: Migrate `MethodBuilderUtils` to Element2 API

--- lib/src/util/method_builder_utils.dart
+++ lib/src/util/method_builder_utils.dart
@@ -1,31 +1,43 @@
-import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:code_builder/code_builder.dart';
 
 extension MethodBuilderUtils on MethodBuilder {
   void copyParameters({
-    required MethodElement method,
+    required MethodElement2 method,
     required Reference Function(DartType type) obtainReferenceForType,
   }) =>
       this
         ..requiredParameters.addAll(
-          method.parameters.where((param) => param.isRequired).map(
-                (parameter) => Parameter(
-                  (builder) => builder
-                    ..name = parameter.name
-                    ..named = parameter.isNamed
-                    ..type = obtainReferenceForType(parameter.type),
-                ),
-              ),
+          method.formalParameters.where((param) => param.isRequired).map(
+            (parameter) {
+              final parameterName = parameter.name3;
+              if (parameterName == null) {
+                return null;
+              }
+              return Parameter(
+                (builder) => builder
+                  ..name = parameterName
+                  ..named = parameter.isNamed
+                  ..type = obtainReferenceForType(parameter.type),
+              );
+            },
+          ).nonNulls,
         )
         ..optionalParameters.addAll(
-          method.parameters.where((param) => param.isOptional).map(
-                (parameter) => Parameter(
-                  (builder) => builder
-                    ..name = parameter.name
-                    ..named = parameter.isNamed
-                    ..type = obtainReferenceForType(parameter.type),
-                ),
-              ),
+          method.formalParameters.where((param) => param.isOptional).map(
+            (parameter) {
+              final parameterName = parameter.name3;
+              if (parameterName == null) {
+                return null;
+              }
+              return Parameter(
+                (builder) => builder
+                  ..name = parameterName
+                  ..named = parameter.isNamed
+                  ..type = obtainReferenceForType(parameter.type),
+              );
+            },
+          ).nonNulls,
         );
 }
