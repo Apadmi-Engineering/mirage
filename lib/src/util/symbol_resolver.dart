b5005d94d9b11063953c8376f092c25dee7c60e2
Tom Ha <tomh@apadmi.com>
2025-11-18 14:12:02 +0000

    feat: Implement `SymbolResolver`

---
+++ lib/src/util/symbol_resolver.dart
@@ -0,0 +1,29 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:code_builder/code_builder.dart';
+
+class SymbolResolver extends RecursiveAstVisitor {
+  static const _coreLibraryScheme = "dart";
+
+  Set<Reference> _references = {};
+
+  @override
+  visitNamedType(NamedType node) {
+    final element = node.element2;
+    if (element == null) {
+      return;
+    }
+    final (name, uri) = (element.name3, element.library2?.uri);
+    if (name == null || uri == null || uri.scheme == _coreLibraryScheme) {
+      return null;
+    }
+    _references.add(Reference(name, uri.toString()));
+    return super.visitNamedType(node);
+  }
+
+  Set<Reference> consumeReferences() {
+    final finalisedReferences = _references.toSet();
+    _references.clear();
+    return finalisedReferences;
+  }
+}
