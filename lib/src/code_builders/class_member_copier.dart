cfc4c9daa1c16a1c5a6bcc0e3d07607e2ad07509
Tom Ha <tomh@apadmi.com>
2025-11-18 14:49:44 +0000

    feat: Implement `ClassMemberCopier`

--- lib/src/code_builders/class_member_copier.dart
+++ lib/src/code_builders/class_member_copier.dart
@@ -1,12 +1,28 @@
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element2.dart';
 import 'package:code_builder/code_builder.dart' hide Expression;
 import 'package:mirage/src/util/object_utils.dart';
+import 'package:mirage/src/util/symbol_resolver.dart';
 
-class FieldCopier {
+abstract interface class MemberCopier {
+  Method? copyGetter(GetterElement element);
+
+  Field? copyField(FieldElement2 element);
+
+  Method? copyMethod(MethodElement2 element);
+}
+
+class MemberCopierImpl implements MemberCopier {
+
+  final ResolvedLibraryResult library;
+
+  MemberCopierImpl(this.library);
+
+  @override
   Method? copyGetter(GetterElement element) =>
-      _copyGetterImplementation(element)?.let((implementation) {
+      copyGetterImplementation(element)?.let((implementation) {
         return Method((mb) => mb
           ..name = element.name3
           ..type = MethodType.getter
@@ -14,8 +30,9 @@ class FieldCopier {
           ..body = implementation);
       });
 
+  @override
   Field? copyField(FieldElement2 element) =>
-      _copyFieldImplementation(element)?.let((implementation) {
+      copyFieldImplementation(element)?.let((implementation) {
         return Field((fb) => fb
           ..name = element.name3
           ..late = element.isLate
@@ -24,19 +41,15 @@ class FieldCopier {
           ..assignment = implementation);
       });
 
+  @override
   Method? copyMethod(MethodElement2 element) =>
-      _copyMethodImplementation(element)?.let((implementation) {
+      copyMethodImplementation(element, library)?.let((implementation) {
         return Method((mb) => mb
           ..name = element.name3
           ..body = implementation);
       });
 
-  Code? _copyGetterImplementation(TypeParameterizedElement2 element) {
-    final library =
-        element.session?.getParsedLibraryByElement2(element.library2);
-    if (library is! ParsedLibraryResult) {
-      return null;
-    }
+  Code? copyGetterImplementation(TypeParameterizedElement2 element) {
     final declaration =
         library.getFragmentDeclaration(element.firstFragment)?.node;
     if (declaration == null) {
@@ -60,7 +73,7 @@ class FieldCopier {
     return bodySource;
   }
 
-  Code? _copyFieldImplementation(FieldElement2 element) {
+  Code? copyFieldImplementation(FieldElement2 element) {
     final library =
         element.session?.getParsedLibraryByElement2(element.library2);
     if (library is! ParsedLibraryResult) {
@@ -82,28 +95,25 @@ class FieldCopier {
     });
   }
 
-  Code? _copyMethodImplementation(MethodElement2 element) {
-    final library =
-        element.session?.getParsedLibraryByElement2(element.library2);
-    if (library is! ParsedLibraryResult) {
-      return null;
-    }
+  Code? copyMethodImplementation(MethodElement2 element, ResolvedLibraryResult resolvedLibrary) {
     final declaration =
-        library.getFragmentDeclaration(element.firstFragment)?.node;
+        resolvedLibrary.getFragmentDeclaration(element.firstFragment)?.node;
     if (declaration == null) {
       return null;
     }
     if (declaration is! MethodDeclaration) {
       return null;
     }
+    final allocator = SymbolResolver();
+    declaration.accept(allocator);
+    final references = allocator.consumeReferences();
     final body = declaration.body;
     return Code.scope((allocate) {
-      SourceRe
-      return body.toSource() ?? "";
+      String bodySource = body.toSource();
+      for (final reference in references) {
+        bodySource = bodySource.replaceAll(reference.symbol ?? "", allocate(reference));
+      }
+      return bodySource;
     });
   }
-}
-
-class MethodSymbolAllocator extends AstVisitor {
-
 }
