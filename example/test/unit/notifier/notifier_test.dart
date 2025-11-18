c41bf2609f35d2473142821ad1fcf1041da565d9
Tom Ha <tomh@apadmi.com>
2025-11-18 11:51:36 +0000

    docs: Add additional assertion in example unit test

--- example/test/unit/notifier/notifier_test.dart
+++ example/test/unit/notifier/notifier_test.dart
@@ -3,6 +3,7 @@ import 'package:mirage_example/src/notifiers/notifier/notifier.dart';
 import 'package:mockito/mockito.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
 
 import 'notifier_test.mirage.dart';
 
@@ -17,9 +18,10 @@ void main() async {
     ]);
 
     // Run test
-    await container.read(dummyProvider(6).future);
+    final result = await container.read(dummyProvider(6).future);
 
     // Verify
+    expect(result, 36);
     final mockDummySource = container.read(dummySourceProvider(6).notifier) as MockDummySource;
     verify(mockDummySource.someSideEffect()).called(1);
   });
