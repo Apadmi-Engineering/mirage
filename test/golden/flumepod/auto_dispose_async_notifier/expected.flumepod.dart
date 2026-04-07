eb1a413c6d9414549a336f41182ad84d55ae4a95
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2026-04-07 08:08:25 +0100

    fix: Fix notifiers sometimes not staying alive in tests (#25)
    * fix: Always generate keep alive insert in notifiers
    
    * test: Update golden tests +  unit tests

--- test/golden/flumepod/auto_dispose_async_notifier/expected.flumepod.dart
+++ test/golden/flumepod/auto_dispose_async_notifier/expected.flumepod.dart
@@ -33,6 +33,8 @@ class MockTestNotifier extends _i1.$AsyncNotifier<String>
 
   @override
   _i4.FutureOr<String> build() async {
+    // keepAlive() used so mock instance with stubbed/real calls isn't disposed.
+    ref.keepAlive();
     return noSuchMethod(Invocation.method(#build, []),
         returnValueForMissingStub: seedBuilder());
   }
