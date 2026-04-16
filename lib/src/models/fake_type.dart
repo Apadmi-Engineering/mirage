a611e50307443e8c07e3b69324e7534cfa54575f
Tom Ha <tomh@apadmi.com>
2026-04-16 15:13:51 +0100

    feat: Implement support for record returns on mocked methods

--- lib/src/models/fake_type.dart
+++ lib/src/models/fake_type.dart
@@ -1,5 +1,8 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:flumepod/src/import_finder.dart';
+import 'package:flumepod/src/util/object_utils.dart';
 
 class FakeType {
   final Element? element;
@@ -10,23 +13,65 @@ class FakeType {
   const FakeType(
     this.element,
     this.originalType,
-    this.fakeTypeName,
+    this.fakeTypeName, {
     this.parameterTypes,
-  );
-
-  String get interfaceName {
-    final localParameterTypes = parameterTypes;
-    String parameters = "";
-    if (localParameterTypes != null && localParameterTypes.isNotEmpty) {
-      parameters += "<";
-      parameters += localParameterTypes
-          .map((parameter) => parameter.interfaceName)
-          .join(", ");
-      parameters += ">";
-    }
-    return "${originalType.element?.displayName}$parameters";
-  }
+  });
 
   @override
-  String toString() => "FakeType(element: $element, parameterTypes: $parameterTypes, fakeTypeName: $fakeTypeName, originalType: $originalType";
+  String toString() =>
+      "FakeType(element: $element, parameterTypes: $parameterTypes, "
+      "fakeTypeName: $fakeTypeName, originalType: $originalType"
+      ")";
+
+  String allocateType(String Function(Reference) allocate, ImportFinder importFinder) {
+    final parameterTypes = this.parameterTypes;
+    final topLevelTypeCode = allocate(refer(
+      originalType.element!.name!,
+      importFinder.getImportUrl(originalType.element!.library),
+    ));
+    if (parameterTypes == null || parameterTypes.isEmpty) {
+      return topLevelTypeCode;
+    }
+    final parameterTypeCode = parameterTypes.map((type) => allocateType(allocate, importFinder)).join(", ");
+    return "$topLevelTypeCode<$parameterTypeCode>";
+  }
+}
+
+class RecordFakeType extends FakeType {
+  final List<FakeType>? positionalFields;
+  final Map<String, FakeType>? namedFields;
+
+  RecordFakeType(
+    super.element,
+    super.originalType,
+    super.fakeTypeName, {
+    this.positionalFields,
+    this.namedFields,
+  });
+
+  @override
+  String toString() =>
+      "RecordFakeType(element: $element, parameterTypes: $parameterTypes, "
+      "fakeTypeName: $fakeTypeName, originalType: $originalType, "
+      "namedFields: $namedFields, positionalFields: $positionalFields"
+      ")";
+
+  @override
+  String allocateType(String Function(Reference) allocate, ImportFinder importFinder) {
+    final (localPositionalFields, localNamedFields) = (positionalFields, namedFields);
+    if(localPositionalFields == null && localNamedFields == null) {
+      return "()";
+    }
+    final positionalFieldsCode = localPositionalFields?.map(
+        (field) => field.allocateType(allocate, importFinder)
+    ).join(",") ?? "";
+    final namedFieldsCode = localNamedFields?.entries.map(
+        (entry) => "${entry.value.allocateType(allocate, importFinder)} ${entry.key}"
+    ).join(",") ?? "";
+    final fieldsCode = [
+      ?positionalFieldsCode.takeIf(positionalFieldsCode.isNotEmpty),
+      ?namedFieldsCode.takeIf(namedFieldsCode.isNotEmpty),
+    ].join(",");
+    return "($fieldsCode)";
+  }
 }
